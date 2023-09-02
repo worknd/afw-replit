b4368ba3031d1c498fa7f6607f5c0b385becc7d0
WorkND <11946531+worknd@users.noreply.github.com>
2023-09-02 13:49:34 +0300

    Update make_url.sh

--- make_url.sh
+++ make_url.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 IFS= read -r password < passwd.txt
 
 domain="${REPL_SLUG}.${REPL_OWNER}.repl.co"
-ss="ss://$(echo -n chacha20-ietf-poly1305:${password} | base64 -w 0)@${domain}:443"
+ss="ss://$(echo -n chacha20-ietf-poly1305:${password} | base64 -w 0)@${domain}:8488"
 
 echo "$ss" > url.txt
